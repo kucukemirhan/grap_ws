10b54e02098fccd5a17a661b2237ca9c7d1d4b0d
Emirhan <emirhankk2@gmail.com>
2025-03-13 20:46:18 +0300

    initial commit

---
+++ build/grap_controller/CMakeFiles/grap_controller.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grap_controller.dir/src/grap_interface.cpp.o"
+  "CMakeFiles/grap_controller.dir/src/grap_interface.cpp.o.d"
+  "libgrap_controller.pdb"
+  "libgrap_controller.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/grap_controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/grap_controller/CMakeFiles/grap_controller.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for grap_controller.
+# This may be replaced when dependencies are built.
